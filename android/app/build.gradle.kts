fe481db35bc3399ec9d94c8f0c39c14908183ff0
fishku <isaacbd@gmail.com>
2026-07-11 19:23:52 +0200

    Share activity views across orientations

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = getGitCommitCount()
-        versionName = "1.2.2"
+        versionName = "1.2.3"
 
         buildConfigField("String", "GIT_HASH", "\"${getGitHash()}\"")
     }
